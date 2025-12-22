97f680fb3b226ef30a24c4c83437a35831e0d1e7
AfolabiOlajide <samolajide20@gmail.com>
2025-12-22 21:44:59 +0100

    updated and refactored protfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,15 +3,7 @@ import React from "react";
 import { FiExternalLink } from "react-icons/fi";
 
 import "./Portfolio.css";
-import img1 from "../../assets/projects-img/weatherinfo-shot.png";
-import img2 from "../../assets/projects-img/dashboard-demo-shot.png";
-import img3 from "../../assets/projects-img/sunny-cosmetics-shot.png";
-import img4 from "../../assets/projects-img/color-library-shot.png";
-import img5 from "../../assets/projects-img/redux-fakestore-snapshot.png";
-import Darate from "../../assets/projects-img/darate_home.png";
-import Youdentify from "../../assets/projects-img/youdentify_home.png";
-import SofGo from "../../assets/projects-img/sofgo_home.png";
-import Decoder from "../../assets/projects-img/decoder_home.png";
+import { PROJECTS } from "../../projects";
 
 const Portfolio = () => {
     return (
@@ -20,253 +12,34 @@ const Portfolio = () => {
             <h2>Portfolio</h2>
 
             <div className="container portfolio__container">
-                {/* Sofgo */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={SofGo} alt="" />
-                    </div>
-                    <h3>SofGo</h3>
-                    <p>
-                        Web Application built for a software development
-                        company.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://sofgo.io/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
-                {/* Darate */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={Darate} alt="" />
-                    </div>
-                    <h3>Darate</h3>
-                    <p>
-                        A Web3 crowdfunding application, built with Next.js,
-                        Typescript, Solidity and Tailwind css.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/darate"
-                            className="btn"
-                            target="_blank"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://darate.vercel.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
-                {/* Youdentify */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={Youdentify} alt="" />
-                    </div>
-                    <h3>YoudentiFi</h3>
-                    <p>
-                        A Web3 Digital Identity Platform, built with Next.js,
-                        Typescript, Tailwind css, Solidity and thirdweb.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/youdentify"
-                            className="btn"
-                            target="_blank"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://youdentify.vercel.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
-                {/* weather info */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img1} alt="" />
-                    </div>
-                    <h3>Weather Info</h3>
-                    <p>
-                        A Weather application where you can get real-time
-                        information on the current weather in any given area.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/weatherInfo-v2"
-                            className="btn"
-                            target="_blank"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://weatherinfo-v2.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
-                {/* fake store */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img5} alt="" />
-                    </div>
-                    <h3>Fake Store</h3>
-                    <p>
-                        This is an E-commerce application built with React,
-                        Redux and Tailwind CSS. It projects major
-                        functionalities of E-commerce applications.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/Redux-Ecommerce-application"
-                            className="btn"
-                            target="_blank"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://redux-fakestore.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
-                {/* multi-decoder */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={Decoder} alt="" />
-                    </div>
-                    <h3>Multi-Decoder</h3>
-                    <p>
-                        An application that allows you to encode and decode text
-                        using different encryption algorithms (open source).
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/multi-decoder"
-                            className="btn"
-                            target="_blank"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://multi-decoder.vercel.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
-                {/* dashboard design */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img2} alt="" />
-                    </div>
-                    <h3>Admin Dashboard Design</h3>
-                    <p>
-                        A Fictional admin dashboard design built for a fictional
-                        E-commerce application.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/dashboard-design"
-                            className="btn"
-                            target="_blank"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://cipherr-dashboard-demo.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
-                {/* sunny cosmetics */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img3} alt="" />
-                    </div>
-                    <h3>Sunny Cosmetics</h3>
-                    <p>
-                        A landing page created for a fictional cosmetics
-                        E-commerce website.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/sunny-cosmetics"
-                            className="btn"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://cipherr-sunny-cosmetics.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                        >
-                            Explore
-                        </a>
-                    </div>
-                </article>
-                {/* color library */}
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img4} alt="" />
-                    </div>
-                    <h3>Color Library</h3>
-                    <p>
-                        A color palette project where you can choose, create and
-                        get any color of your choice.
-                    </p>
-                    <div className="portfolio__item-cta">
-                        <a
-                            href="https://github.com/AfolabiOlajide/Color-Library"
-                            className="btn"
-                            target="_blank"
-                        >
-                            GitHub
-                        </a>
-                        <a
-                            href="https://color-library.netlify.app/"
-                            className="btn btn-primary"
-                            target="_blank"
-                        >
-                            Explore
-                            <FiExternalLink />
-                        </a>
-                    </div>
-                </article>
+                {PROJECTS.map((project) => (
+                    <article id={project.id} className="portfolio__item">
+                        <div className="portfolio__item-image">
+                            <img src={project.imgSrc} alt="" />
+                        </div>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <div className="portfolio__item-cta">
+                            {project.githubLink && (
+                                <a
+                                    href={project.githubLink}
+                                    className="btn"
+                                    target="_blank"
+                                >
+                                    GitHub
+                                </a>
+                            )}
+                            <a
+                                href={project.demoLink}
+                                className="btn btn-primary"
+                                target="_blank"
+                            >
+                                Explore
+                                <FiExternalLink />
+                            </a>
+                        </div>
+                    </article>
+                ))}
             </div>
         </section>
     );
---
+++ src/projects.js
@@ -0,0 +1,95 @@
+import { v4 as uuidv4 } from "uuid";
+import img1 from "./assets/projects-img/weatherinfo-shot.png";
+import img2 from "./assets/projects-img/dashboard-demo-shot.png";
+import img3 from "./assets/projects-img/sunny-cosmetics-shot.png";
+import img4 from "./assets/projects-img/color-library-shot.png";
+import img5 from "./assets/projects-img/redux-fakestore-snapshot.png";
+import Darate from "./assets/projects-img/darate_home.png";
+import Youdentify from "./assets/projects-img/youdentify_home.png";
+import SofGo from "./assets/projects-img/sofgo_home.png";
+import Decoder from "./assets/projects-img/decoder_home.png";
+
+export const PROJECTS = [
+    {
+        id: uuidv4(),
+        imgSrc: SofGo,
+        title: "SofGo",
+        description:
+            "Web Application built for a software development company.",
+        githubLink: "",
+        demoLink: "https://sofgo.io/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: Darate,
+        title: "Darate",
+        description:
+            "A Web3 crowdfunding application, built with Next.js, Typescript, Solidity and Tailwind css.",
+        githubLink: "https://github.com/AfolabiOlajide/darate",
+        demoLink: "https://darate.vercel.app/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: Youdentify,
+        title: "YoudentiFi",
+        description:
+            "A Web3 Digital Identity Platform, built with Next.js, Typescript, Tailwind css, Solidity and thirdweb.",
+        githubLink: "https://github.com/AfolabiOlajide/youdentify",
+        demoLink: "https://youdentify.vercel.app/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: img1,
+        title: "Weather Info",
+        description:
+            "A Weather application where you can get real-time information on the current weather in any given area.",
+        githubLink: "https://github.com/AfolabiOlajide/weatherInfo-v2",
+        demoLink: "https://weatherinfo-v2.netlify.app/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: img5,
+        title: "Fake Store",
+        description:
+            "This is an E-commerce application built with React, Redux and Tailwind CSS. It projects major functionalities of E-commerce applications.",
+        githubLink:
+            "https://github.com/AfolabiOlajide/Redux-Ecommerce-application",
+        demoLink: "https://redux-fakestore.netlify.app/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: Decoder,
+        title: "Multi-Decoder",
+        description:
+            "An application that allows you to encode and decode text using different encryption algorithms (open source).",
+        githubLink: "https://github.com/AfolabiOlajide/multi-decoder",
+        demoLink: "https://multi-decoder.vercel.app/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: img2,
+        title: "Admin Dashboard Design",
+        description:
+            "A Fictional admin dashboard design built for a fictional E-commerce application.",
+        githubLink: "https://github.com/AfolabiOlajide/dashboard-design",
+        demoLink: "https://cipherr-dashboard-demo.netlify.app/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: img3,
+        title: "Sunny Cosmetics",
+        description:
+            "A landing page created for a fictional cosmetics E-commerce website.",
+        githubLink: "https://github.com/AfolabiOlajide/sunny-cosmetics",
+        demoLink: "https://cipherr-sunny-cosmetics.netlify.app/",
+    },
+    {
+        id: uuidv4(),
+        imgSrc: img4,
+        title: "Color Library",
+        description:
+            "A color palette project where you can choose, create and get any color of your choice.",
+        githubLink: "https://github.com/AfolabiOlajide/Color-Library",
+        demoLink: "https://color-library.netlify.app/",
+    },
+];
